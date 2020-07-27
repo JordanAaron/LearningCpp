1e8e4ac1b0df94ec3816e8abe95b8058790a4c00
Jordan <jordev14@gmail.com>
2020-07-27 17:47:28 +0100

    Ternary Operator
    Shortening the if else statement and storing the result in a variable. Not exclusive to c++ but I want to use this more often.

--- C++Course/MyFirstProgram.cpp
+++ C++Course/MyFirstProgram.cpp
@@ -1,35 +1,10 @@
+#include <cmath>
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-string greeting = "Hello";
-string alphabet = "abcdefghijklmnopqrstuvwxyz";
-string s = "Bring me bread";
-string str = "ABAABACCABA";
-string fullName;
-
-
 int main() {
-
-	//Can concatenate with '+' or 'append()'
-	cout << greeting << endl;
-	greeting.append(" World");
-	cout << greeting << endl;
-
-	//Can use 'length()' or 'size()' to retrieve a strings length
-	cout << alphabet.length() << endl;
-
-	//Accessing characters and changing characters
-	cout << s[0] << endl;
-	s[0] = 'G';
-	cout << s << endl;
-
-	//User Input
-	cout << "Type your full name: ";
-	//getline(cin, fullName);
-	cin >> fullName;
-	cout << "Your name is: " << fullName;
-
-	return 0;
+	int time = 10;
+	string result = (8 < time < 20) ? "Day time": "Night time";
+	cout << result;
 }
