86263c41cd37ecf0e263d9ec438ddb548ddfbb25
Jordan <jordev14@gmail.com>
2020-07-27 19:56:19 +0100

    References
    We can create references to refer to other variables. Can also access the memory address of a variable using the & operator.

--- C++Course/MyFirstProgram.cpp
+++ C++Course/MyFirstProgram.cpp
@@ -1,10 +1,22 @@
-#include <cmath>
 #include <iostream>
 
 using namespace std;
 
+string food = "Pizza";
+string &meal = food; //Creates a reference variable called meal to the food variable
+					 // 'food' or 'meal' can be used to refer to the value of food
+
 int main() {
-	int time = 10;
-	string result = (8 < time < 20) ? "Day time": "Night time";
-	cout << result;
+	
+	cout << food << endl;
+	cout << meal << endl;
+
+	//Output the memory address
+	cout << &food;
+
+	return 0;
 }
+
+// References and Pointers (which you will learn about in the next chapter) are important
+// in C++, because they give you the ability to manipulate the data in the computer's memory 
+// - which can reduce the code and improve the perfomance.
