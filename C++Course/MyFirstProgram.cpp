3cc2e552e741af00ab892523d494f44e4f1ed9a5
Jordan <jordev14@gmail.com>
2020-07-27 11:19:22 +0100

    Strings
    Concatenation, Length, Access, User Input

--- C++Course/MyFirstProgram.cpp
+++ C++Course/MyFirstProgram.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-float f1 = 35e3;
-double d1 = 12E4;
+string greeting = "Hello";
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+string s = "Bring me bread";
+string str = "ABAABACCABA";
+string fullName;
+
 
 int main() {
-	cout << f1 << endl;
-	cout << d1;
+
+	//Can concatenate with '+' or 'append()'
+	cout << greeting << endl;
+	greeting.append(" World");
+	cout << greeting << endl;
+
+	//Can use 'length()' or 'size()' to retrieve a strings length
+	cout << alphabet.length() << endl;
+
+	//Accessing characters and changing characters
+	cout << s[0] << endl;
+	s[0] = 'G';
+	cout << s << endl;
+
+	//User Input
+	cout << "Type your full name: ";
+	//getline(cin, fullName);
+	cin >> fullName;
+	cout << "Your name is: " << fullName;
+
 	return 0;
 }
